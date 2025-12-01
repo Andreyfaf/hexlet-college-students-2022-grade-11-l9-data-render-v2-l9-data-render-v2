c515f0842d623d3c6a27d04a7a0d3b8a44baa513
Andrey.faf <147048858+Andreyfaf@users.noreply.github.com>
2025-12-01 10:50:34 +0300

    Update CafesTable.jsx
    done

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+const FilterCafes = ({ stations }) => {
+    return (
+      <div className="controls">
+        <select name="subway" id="subway">
+          <option value="All">Все</option>
+          {stations.map(station => (
+            <option key={station.code} value={station.code}>{station.name}</option>
+          ))}
+        </select>
+      </div>
+    );
+  };
+  
+  export default FilterCafes;
