f005c7007c6f5e8fe81016408a7ec309ae134117
Andrey.faf <147048858+Andreyfaf@users.noreply.github.com>
2025-12-01 10:51:28 +0300

    Update CafesTable.jsx
    Done

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FilterCafes from './FilterCafes'; 
+import axios from 'axios';
 
-const FilterCafes = ({ stations }) => {
+const stations = [
+    { name: "Арбатская", code: "Arbat" },
+    { name: "Александровский сад", code: "Alexanders Garden" },
+    { name: "Московская", code: "Moscow" },
+    { name: "Парк Культуры", code: "Culture" },
+    { name: "Театральная", code: "Theater" }
+  ];
+
+
+
+  const CafesTable = () => {
+    const [cafes, setCafes] = useState([]);
+  
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await axios.get('/cafes'); 
+          setCafes(response.data); 
+        } catch (err) {
+          console.error('Ошибка загрузки данных:', err.message);
+        }
+      }
+      fetchData(); 
+    }, []); 
     return (
-      <div className="controls">
-        <select name="subway" id="subway">
-          <option value="All">Все</option>
-          {stations.map(station => (
-            <option key={station.code} value={station.code}>{station.name}</option>
+      <div className="cafesTable">
+        <FilterCafes stations={stations} /> {}
+        <ul className="cardsList">
+          {cafes.map((cafe, index) => (
+            <li key={index} className="card">
+              <img src={cafe.image ? cafe.image : "https://via.placeholder.com/150"} alt={cafe.name} />
+              <h2>{cafe.name}</h2>
+              <p>{cafe.description || ''}</p>
+              <p>{cafe.address || ''}</p>
+              <p>Subway: {cafe.subway || '—'}</p>
+              <p>{cafe.openingHours || ''}</p>
+            </li>
           ))}
-        </select>
+        </ul>
       </div>
     );
   };
   
-  export default FilterCafes;
+  export default CafesTable;
